a8e3493e6e2ae7de91e14835803998a2ae676619
mekain <rhfnajfl0@naver.com>
2025-09-05 01:02:35 +0900

    [수영] #플레이어 이동 속도 수정

--- Project1/config.h
+++ Project1/config.h
@@ -14,7 +14,7 @@ constexpr int    kPlayerStartPosY = dfSCREEN_HEIGHT - 1;
 constexpr int    kFps = 50;
 constexpr double kFrameTimeMs = 1000.0 / kFps;  // 1프레임 = 약 20ms
 constexpr double kFrameTimeSec = 1.0 / kFps;
-constexpr int    kPlayerMoveSpeedCellsPerFrame = 7;
+constexpr int    kPlayerMoveSpeedCellsPerFrame = 5;
 constexpr int    kPlayerAttackSpeed = 8;
 constexpr int    kPlayerInvincibleFrames = 30; // 데미지 받고 몇 프레임 무적으로 할것인지
 constexpr char   kPlayerInvincibleShape = 'B';
