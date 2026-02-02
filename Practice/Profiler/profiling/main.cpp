28f777f5a55c4bf3f02aca1eb562ab8fe47c04a6
mekain <rhfnajfl0@naver.com>
2026-02-03 01:12:22 +0900

    [메카인] #Profile 프로젝트 추가

---
+++ Practice/Profiler/profiling/main.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <wchar.h>
+#include "profiling.h"
+
+WCHAR currTimestamp[32];
+
+void MakeTimestamp()
+{
+	SYSTEMTIME st;
+	GetLocalTime(&st);
+
+	// YYYYMMDD_HHMMSS 형태
+	swprintf_s(currTimestamp, 32, L"%04d%02d%02d_%02d%02d%02d",
+		st.wYear, st.wMonth, st.wDay,
+		st.wHour, st.wMinute, st.wSecond);
+}
+
+int add1(int a, int b) {
+	ProfileBegin(L"add1");
+	Sleep(1);
+
+	int ret = a + b;
+	ProfileEnd(L"add1");
+	return ret;
+}
+
+int add10000(int a, int b) {
+	ProfileBegin(L"add10000");
+
+	int total = 0;
+	for (size_t i = 0; i < 10000; i++)
+	{
+		total += (a + b);
+	}
+	ProfileEnd(L"add10000");
+
+	return total;
+}
+
+struct A {
+	int a;
+	int b;
+	char c;	
+};
+
+void structCopy() {
+	ProfileBegin(L"structCopy");
+	A a{ 1, 2, 'x' };
+	A b = a;
+	ProfileEnd(L"structCopy");
+}
+
+#pragma function(memcpy)             // ← 인트린식 해제 (이 파일/구간에만)
+__declspec(noinline)                 // 인라이닝 방지 (어셈 확인 용이)
+void memcpyFunc() {
+	ProfileBegin(L"memcpyFunc");
+	A a{ 1, 2, 'x' };
+	A b;
+	memcpy(&b, &a, sizeof(A));
+	ProfileEnd(L"memcpyFunc");
+}
+
+int main() {
+	ProfileReset();
+	//for (size_t i = 0; i < 10; i++) {
+	//	std::cout << i << std::endl;
+	//	add1(i, i);
+	//}
+	//for (size_t i = 0; i < 1000; i++) add10000(i, i);
+	//for (size_t i = 0; i < 10000; i++) structCopy();
+	//for (size_t i = 0; i < 10000; i++) memcpyFunc();
+	structCopy();
+	memcpyFunc();
+	char c = 'c';
+	int a = 1;
+	int b = 2;
+	if (a < b) {
+		std::cout << a;
+	}
+	else if (a <= b) {
+		std::cout << a;
+	}
+
+	MakeTimestamp();
+
+	WCHAR fileName[MAX_PATH];
+	swprintf_s(fileName, L"%s%ls.txt", PROFILE_FILE_NAME, currTimestamp);
+	ProfileDataOutText(fileName);
+
+	// 확인 출력
+	char utf8[256];
+	int len = WideCharToMultiByte(CP_UTF8, 0, fileName, -1, utf8, sizeof(utf8), NULL, NULL);
+	if (len > 0) {
+		printf("Saved: %s\n", utf8);
+	}
+}
+
---
+++ Practice/Profiler/profiling/profiling.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include <windows.h>
+#include <string.h>
+#include <stdio.h>
+#include <limits>
+
+const static int PROFILE_MAX_CNT = 100;
+const static int PROFILE_NAME_LEN = 65;
+const static int PROFILE_DELETE_CNT = 2;
+const static WCHAR PROFILE_FILE_NAME[] = L"profiling_";
+
+struct profile
+{
+	long			mFlag;								// 프로파일의 사용 여부. (배열시에만)
+	WCHAR			mName[PROFILE_NAME_LEN];			// 프로파일 샘플 이름.
+
+	LARGE_INTEGER	mStartTime;							// 프로파일 샘플 실행 시간.
+
+	__int64			mTotalTime;							// 전체 사용시간 카운터 Time.	(출력시 호출회수로 나누어 평균 구함)
+	__int64			mMin[PROFILE_DELETE_CNT];			// 최소 사용시간 카운터 Time.	(초단위로 계산하여 저장 / [0] 가장최소 [1] 다음 최소 [2])
+	__int64			mMax[PROFILE_DELETE_CNT];			// 최대 사용시간 카운터 Time.	(초단위로 계산하여 저장 / [0] 가장최대 [1] 다음 최대 [2])
+
+	__int64			mCall;								// 누적 호출 횟수.
+};
+
+extern profile gProfileArray[PROFILE_MAX_CNT];
+extern LARGE_INTEGER gFreq;
+
+// 하나의 함수 Profiling 시작
+// Parameters: Profiling이름.
+void ProfileBegin(const WCHAR* mName);
+void ProfileEnd(const WCHAR* mName);
+
+// Profiling 된 데이터를 Text 파일로 출력
+//
+// Parameters: (char *)출력될 파일 이름.
+void ProfileDataOutText(const WCHAR* szFileName);
+
+// 프로파일링 된 데이터를 모두 초기화
+void ProfileReset(void);
