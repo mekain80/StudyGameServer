36ccaf3ddd8d9835ac3c58b97ac8cb0f9238ed3f
mekain <rhfnajfl0@naver.com>
2026-02-15 15:26:30 +0900

    [메카인] #직렬화 버퍼 수정 전 업로드

---
+++ Practice/SerializedBuffer/SerializedBuffer/SerializedBuffer.cpp
@@ -0,0 +1,242 @@
+﻿#include "SerializedBuffer.h"
+#include <cstring>   // memcpy, memmove
+#include <algorithm> // min
+
+SerializedBuffer::SerializedBuffer() : SerializedBuffer(eBUFFER_DEFAULT)
+{
+}
+
+
+SerializedBuffer::SerializedBuffer(int iBUfferSize)
+	: mBufferSize(iBUfferSize),
+	mDataSize(0)
+{
+	mBuffer = new char[mBufferSize];
+}
+
+SerializedBuffer::~SerializedBuffer()
+{
+	delete[] mBuffer;
+}
+
+void SerializedBuffer::Clear()
+{
+	mDataSize = 0;
+}
+
+int SerializedBuffer::MoveWritePos(int iSize)
+{
+	if (iSize <= 0)
+		return 0;
+
+	// 남은 공간보다 더 많이 이동하라고 하면 하지 않음
+	int freeSize = mBufferSize - mDataSize;
+	if (freeSize < iSize)
+		return 0;
+
+	mDataSize += iSize;
+	return iSize;
+}
+
+int SerializedBuffer::MoveReadPos(int iSize)
+{
+	if (iSize <= 0)
+		return 0;
+
+	int moveSize = min(iSize, mDataSize);
+
+	// 사용된 데이터 앞으로 땡기기
+	int remain = mDataSize - moveSize;
+	if (remain > 0)
+	{
+		std::memmove(mBuffer,
+			mBuffer + moveSize,
+			remain);
+	}
+
+	mDataSize -= moveSize;
+	return moveSize;
+}
+
+/// 대입 연산자
+SerializedBuffer& SerializedBuffer::operator=(SerializedBuffer& clSrcPacket)
+{
+	if (this == &clSrcPacket)
+		return *this;
+
+	// 버퍼 크기가 다르면 다시 할당
+	if (mBufferSize != clSrcPacket.mBufferSize)
+	{
+		delete[] mBuffer;
+		mBufferSize = clSrcPacket.mBufferSize;
+		mBuffer = new char[mBufferSize];
+	}
+
+	mDataSize = clSrcPacket.mDataSize;
+
+	// 실제 들어있는 데이터만 복사
+	if (mDataSize > 0)
+	{
+		std::memcpy(mBuffer,
+			clSrcPacket.mBuffer,
+			mDataSize);
+	}
+
+	return *this;
+}
+
+// ======================= Put 연산자 (<<) =======================
+
+SerializedBuffer& SerializedBuffer::operator<<(unsigned char byValue)
+{
+	PutData(reinterpret_cast<char*>(&byValue), sizeof(byValue));
+	return *this;
+}
+
+SerializedBuffer& SerializedBuffer::operator<<(char chValue)
+{
+	PutData(&chValue, sizeof(chValue));
+	return *this;
+}
+
+SerializedBuffer& SerializedBuffer::operator<<(short shValue)
+{
+	PutData(reinterpret_cast<char*>(&shValue), sizeof(shValue));
+	return *this;
+}
+
+SerializedBuffer& SerializedBuffer::operator<<(unsigned short wValue)
+{
+	PutData(reinterpret_cast<char*>(&wValue), sizeof(wValue));
+	return *this;
+}
+
+SerializedBuffer& SerializedBuffer::operator<<(int iValue)
+{
+	PutData(reinterpret_cast<char*>(&iValue), sizeof(iValue));
+	return *this;
+}
+
+SerializedBuffer& SerializedBuffer::operator<<(long lValue)
+{
+	PutData(reinterpret_cast<char*>(&lValue), sizeof(lValue));
+	return *this;
+}
+
+SerializedBuffer& SerializedBuffer::operator<<(float fValue)
+{
+	PutData(reinterpret_cast<char*>(&fValue), sizeof(fValue));
+	return *this;
+}
+
+SerializedBuffer& SerializedBuffer::operator<<(__int64 iValue)
+{
+	PutData(reinterpret_cast<char*>(&iValue), sizeof(iValue));
+	return *this;
+}
+
+SerializedBuffer& SerializedBuffer::operator<<(double dValue)
+{
+	PutData(reinterpret_cast<char*>(&dValue), sizeof(dValue));
+	return *this;
+}
+
+// ======================= Get 연산자 (>>) =======================
+
+SerializedBuffer& SerializedBuffer::operator>>(BYTE& byValue)
+{
+	GetData(reinterpret_cast<char*>(&byValue), sizeof(byValue));
+	return *this;
+}
+
+SerializedBuffer& SerializedBuffer::operator>>(char& chValue)
+{
+	GetData(&chValue, sizeof(chValue));
+	return *this;
+}
+
+SerializedBuffer& SerializedBuffer::operator>>(short& shValue)
+{
+	GetData(reinterpret_cast<char*>(&shValue), sizeof(shValue));
+	return *this;
+}
+
+SerializedBuffer& SerializedBuffer::operator>>(WORD& wValue)
+{
+	GetData(reinterpret_cast<char*>(&wValue), sizeof(wValue));
+	return *this;
+}
+
+SerializedBuffer& SerializedBuffer::operator>>(int& iValue)
+{
+	GetData(reinterpret_cast<char*>(&iValue), sizeof(iValue));
+	return *this;
+}
+
+SerializedBuffer& SerializedBuffer::operator>>(DWORD& dwValue)
+{
+	GetData(reinterpret_cast<char*>(&dwValue), sizeof(dwValue));
+	return *this;
+}
+
+SerializedBuffer& SerializedBuffer::operator>>(float& fValue)
+{
+	GetData(reinterpret_cast<char*>(&fValue), sizeof(fValue));
+	return *this;
+}
+
+SerializedBuffer& SerializedBuffer::operator>>(__int64& iValue)
+{
+	GetData(reinterpret_cast<char*>(&iValue), sizeof(iValue));
+	return *this;
+}
+
+SerializedBuffer& SerializedBuffer::operator>>(double& dValue)
+{
+	GetData(reinterpret_cast<char*>(&dValue), sizeof(dValue));
+	return *this;
+}
+
+// ======================= GetData / PutData =======================
+
+int SerializedBuffer::GetData(char* chpDest, int iSize)
+{
+	if (iSize <= 0 || chpDest == nullptr)
+		return 0;
+
+	int copySize = min(iSize, mDataSize);
+	if (copySize <= 0)
+		return 0;
+
+	std::memcpy(chpDest, mBuffer, copySize);
+
+	int remain = mDataSize - copySize;
+	if (remain > 0)
+	{
+		std::memmove(mBuffer,
+			mBuffer + copySize,
+			remain);
+	}
+
+	mDataSize -= copySize;
+	return copySize;
+}
+
+int SerializedBuffer::PutData(char* chpSrc, int iSrcSize)
+{
+	if (iSrcSize <= 0 || chpSrc == nullptr)
+		return 0;
+
+	int freeSize = mBufferSize - mDataSize;
+	if (iSrcSize > freeSize)
+		return 0;
+
+	int copySize = min(iSrcSize, freeSize);
+
+	std::memcpy(mBuffer + mDataSize,
+		chpSrc,
+		copySize);
+
+	mDataSize += copySize;
+	return copySize;
+}
---
+++ Practice/SerializedBuffer/SerializedBuffer/SerializedBuffer.h
@@ -0,0 +1,92 @@
+﻿#pragma once
+#include <Windows.h>
+
+class SerializedBuffer
+{
+public:
+	enum Constants
+	{
+		eBUFFER_DEFAULT = 1024 // 패킷의 기본 버퍼 사이즈.
+	};
+
+	SerializedBuffer();
+	SerializedBuffer(int iBUfferSize);
+	virtual ~SerializedBuffer();
+
+	/// 패킷 청소
+	void Clear();
+
+	/// 버퍼 사이즈 얻기.
+	/// Return: (int)패킷 버퍼 사이즈 얻기.
+	int	GetBufferSize() { return mBufferSize; }
+
+	/// 현재 사용중인 사이즈 얻기.
+	/// Return: (int)사용중인 데이타 사이즈.
+	int GetDataSize() { return mDataSize; }
+
+	/// 버퍼 포인터 얻기.
+	/// Return: (char *)버퍼 포인터.
+	char* GetBufferPtr() { return mBuffer; }
+
+	/// 버퍼 Pos 이동. (음수이동은 안됨)
+	/// GetBufferPtr 함수를 이용하여 외부에서 강제로 버퍼 내용을 수정할 경우 사용. 
+	int		MoveWritePos(int iSize);
+	int		MoveReadPos(int iSize);
+
+	/* ============================================================================= */
+	// 연산자 오버로딩
+	/* ============================================================================= */
+	SerializedBuffer& operator = (SerializedBuffer& clSrcPacket);
+
+	SerializedBuffer& operator << (unsigned char byValue);
+	SerializedBuffer& operator << (char chValue);
+	SerializedBuffer& operator << (unsigned short wValue);
+	SerializedBuffer& operator << (short shValue);
+	SerializedBuffer& operator << (int iValue);
+	SerializedBuffer& operator << (unsigned long lValue);
+	SerializedBuffer& operator << (long lValue);
+	SerializedBuffer& operator << (float fValue);
+	SerializedBuffer& operator << (__int64 iValue);
+	SerializedBuffer& operator << (double dValue);
+
+	//SerializedBuffer& operator<<(std::uint8_t  v) { writePod(v); return *this; }
+	//SerializedBuffer& operator<<(std::int8_t   v) { writePod(v); return *this; }
+	//SerializedBuffer& operator<<(char          v) { writePod(v); return *this; }
+	//SerializedBuffer& operator<<(std::uint16_t v) { writePod(v); return *this; }
+	//SerializedBuffer& operator<<(std::int16_t  v) { writePod(v); return *this; }
+	//SerializedBuffer& operator<<(std::uint32_t v) { writePod(v); return *this; }
+	//SerializedBuffer& operator<<(std::int32_t  v) { writePod(v); return *this; }
+	//SerializedBuffer& operator<<(std::uint64_t v) { writePod(v); return *this; }
+	//SerializedBuffer& operator<<(std::int64_t  v) { writePod(v); return *this; }
+	//SerializedBuffer& operator<<(float         v) { writePod(v); return *this; }
+	//SerializedBuffer& operator<<(float         v) { writePod(v); return *this; }
+	//SerializedBuffer& operator<<(double        v) { writePod(v); return *this; }
+
+
+
+	SerializedBuffer& operator >> (BYTE& byValue);
+	SerializedBuffer& operator >> (char& chValue);
+	SerializedBuffer& operator >> (short& shValue);
+	SerializedBuffer& operator >> (WORD& wValue);
+	SerializedBuffer& operator >> (int& iValue);
+	SerializedBuffer& operator >> (DWORD& dwValue);
+	SerializedBuffer& operator >> (float& fValue);
+	SerializedBuffer& operator >> (__int64& iValue);
+	SerializedBuffer& operator >> (double& dValue);
+
+	/// @breif 데이타 얻기.
+	/// @param (char *)Dest 포인터. (int)Size.
+	/// @return (int)복사한 사이즈.
+	int		GetData(char* chpDest, int iSize);
+
+	/// @breif 데이타 삽입
+	/// @param (char *)Src 포인터. (int)SrcSize.
+	/// @return (int)복사한 사이즈.
+	int		PutData(char* chpSrc, int iSrcSize);
+
+
+private:
+	int   mBufferSize;
+	int   mDataSize;
+	char* mBuffer;
+};
