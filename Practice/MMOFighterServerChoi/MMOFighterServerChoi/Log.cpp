3130d2cf6b310aed07f3d88c2032b7652a1cd8d2
mekain <rhfnajfl0@naver.com>
2026-03-14 20:02:55 +0900

    [메카인] #MMOFighter, 기본 로그 출력 레벨은 SYSTEM으로 수정

--- Practice/MMOFighterServerChoi/MMOFighterServerChoi/Log.cpp
+++ Practice/MMOFighterServerChoi/MMOFighterServerChoi/Log.cpp
@@ -14,7 +14,7 @@ void LogFileInit()
 	time_t timer = time(nullptr); // 현재 시각을 초 단위로
 	localtime_s(&t, &timer); // 초 단위 시간 분리 후, 구조체
 
-	gLogLevel = LOG_LEVEL_DEBUG;
+	gLogLevel = LOG_LEVEL_SYSTEM;
 
 	sprintf_s(FileName, "%s_%4d_%02d_%02d.txt", "Log", 1900 + t.tm_year, t.tm_mon + 1, t.tm_mday);
 }
